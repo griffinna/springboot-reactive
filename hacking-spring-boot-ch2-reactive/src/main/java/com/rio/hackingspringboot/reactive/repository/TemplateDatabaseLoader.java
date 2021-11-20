ee50078c2711d2e40b186e77016a3088f281a74a
Griffin <garam.dev9@gmail.com>
2021-11-20 23:07:24 +0900

    [chapter2] Create DatabaseLoader Class for Item
    - Blocking API
    - MongoOperations

---
+++ hacking-spring-boot-ch2-reactive/src/main/java/com/rio/hackingspringboot/reactive/repository/TemplateDatabaseLoader.java
@@ -0,0 +1,19 @@
+package com.rio.hackingspringboot.reactive.repository;
+
+import com.rio.hackingspringboot.reactive.entity.Item;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TemplateDatabaseLoader {   // MongoOperations 기반 데이터베이스 로더
+
+    @Bean
+    CommandLineRunner initialize(MongoOperations mongo) {
+        return args -> {
+            mongo.save(new Item("Alf alarm clock", 19.99));
+            mongo.save(new Item("Smurf TV tray", 24.99));
+        };
+    }
+}
