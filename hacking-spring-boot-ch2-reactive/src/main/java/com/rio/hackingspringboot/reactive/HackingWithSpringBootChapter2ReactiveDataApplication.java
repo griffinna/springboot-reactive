24b28e35a5d8cb928c647dd8c6abe1bed85ea504
Griffin <garam.dev9@gmail.com>
2021-11-22 21:21:36 +0900

    [chapter3] 블록하운드를 사용한 블로킹 코드 검출

--- hacking-spring-boot-ch2-reactive/src/main/java/com/rio/hackingspringboot/reactive/HackingWithSpringBootChapter2ReactiveDataApplication.java
+++ hacking-spring-boot-ch2-reactive/src/main/java/com/rio/hackingspringboot/reactive/HackingWithSpringBootChapter2ReactiveDataApplication.java
@@ -2,11 +2,21 @@ package com.rio.hackingspringboot.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class HackingWithSpringBootChapter2ReactiveDataApplication {
 
 	public static void main(String[] args) {
+		// 블록하운드 등록
+		// SpringApplication.run 보다 선행되어야 바이트코드를 조작 가능
+		BlockHound.builder()
+				// TemplateEngine.process() 를 블록하운드 허용 리스트에 추가
+				.allowBlockingCallsInside(
+						TemplateEngine.class.getCanonicalName(), "process")
+				.install();
+
 		SpringApplication.run(HackingWithSpringBootChapter2ReactiveDataApplication.class, args);
 	}
 
